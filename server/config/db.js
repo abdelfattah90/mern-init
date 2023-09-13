539364410f173cb709d1cdcbe775838d546a4f5e
Ahmed Abdelfattah <ahmedabdelfattah0022@gmail.com>
2023-09-13 08:59:03 +0300

    mongoose promises

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,26 @@
 import mongoose from 'mongoose'
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI)
-    console.log('DB Connected...')
-  } catch (error) {
-    console.error('DB Failed...', error)
-  }
+import dotenv from 'dotenv'
+dotenv.config()
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      console.log('DB Connected...')
+    })
+    .catch(() => {
+      console.log('DB Failed...')
+    })
 }
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI)
+//     console.log('DB Connected...')
+//   } catch (error) {
+//     console.error('DB Failed...', error)
+//   }
+// }
 
 export default connectDB
